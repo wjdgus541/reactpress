d04e589089c3ab1a9bfc028abfae06e29e1d5e0e
wjdgus541 <nnn5425@naver.com>
2022-01-14 18:25:50 +0900

    Add search func
    어설프지만 암튼 해넀다!

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -3,6 +3,9 @@ import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { motion, AnimatePresence } from "framer-motion";
+import { blogListAtom } from "../recoil";
+import { useRecoilValue } from "recoil";
+import { Link, useHistory } from "react-router-dom";
 
 const SearchForm = styled(motion.form)`
   border-radius: 20px;
@@ -11,6 +14,7 @@ const SearchForm = styled(motion.form)`
   display: flex;
   align-items: center;
 `;
+
 const IconBox = styled(motion.div)`
   cursor: pointer;
   border-radius: 20px;
@@ -29,6 +33,19 @@ const SearchInput = styled.input`
   }
 `;
 
+const SearchList = styled.ul`
+  position: fixed;
+  width: 160px;
+  top: 30px;
+  background-color: white;
+  border: solid 1px gray;
+  list-style: none;
+`;
+
+const SearchItem = styled.li`
+  width: 100%;
+`;
+
 const formVariants = {
   initial: {
     width: 0,
@@ -45,34 +62,95 @@ const formVariants = {
 };
 
 export default function Search() {
+  const [filterList, setfilterList] = useState([]);
   const [searchOpen, setSearchOpen] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
   const inputFocus = useRef();
-  const onSearch = () => {
+  const history = useHistory();
+  const listKeys = Object.keys(useRecoilValue(blogListAtom));
+
+  const onChange = (e) => {
+    setSearchValue(e.target.value);
+    const text = e.target.value;
+    if (text === "") setfilterList([]);
+    else {
+      const regex = new RegExp(text, "gi");
+      const filterArr = listKeys.filter((item) => item.match(regex));
+      setfilterList(filterArr);
+    }
+  };
+  const searchBoxToggle = () => {
     setSearchOpen((prev) => !prev);
   };
+  const outFocus = () => {
+    setfilterList([]);
+    setSearchValue("");
+    setSearchOpen(false);
+  };
+  const onEnter = (e) => {
+    if (e.key === "Enter") {
+      if (filterList[0]) {
+        history.push(`/blog/${filterList[0]}`);
+        setfilterList([]);
+        setSearchValue("");
+      }
+    }
+  };
+  const resetfilter = () => {
+    setfilterList([]);
+    setSearchValue("");
+  };
+
   useEffect(() => {
     if (searchOpen === true) inputFocus.current.focus();
   }, [searchOpen]);
+
   return (
     <>
       <AnimatePresence>
         {searchOpen ? (
-          <SearchForm
-            variants={formVariants}
-            initial="initial"
-            animate="visible"
-            exit="leaving"
-          >
-            <IconBox layoutId="icon">
-              <SearchIcon onClick={onSearch} />
-            </IconBox>
-            <SearchInput ref={inputFocus} type="text" />
-          </SearchForm>
+          <>
+            <SearchForm
+              variants={formVariants}
+              initial="initial"
+              animate="visible"
+              exit="leaving"
+            >
+              <IconBox layoutId="icon">
+                <SearchIcon onClick={searchBoxToggle} />
+              </IconBox>
+              <SearchInput
+                value={searchValue}
+                onChange={onChange}
+                onKeyPress={onEnter}
+                onBlur={outFocus}
+                ref={inputFocus}
+                type="text"
+              />
+            </SearchForm>
+            {filterList.length > 0 ? (
+              <SearchList>
+                {filterList.map((item) => (
+                  <SearchItem key={item}>
+                    <Link
+                      to={{
+                        pathname: `/blog/${item}`,
+                        state: { blogtitle: item },
+                      }}
+                      onClick={resetfilter}
+                    >
+                      {item}
+                    </Link>
+                  </SearchItem>
+                ))}
+              </SearchList>
+            ) : null}
+          </>
         ) : null}
       </AnimatePresence>
       {searchOpen ? null : (
         <IconBox layoutId="icon">
-          <SearchIcon onClick={onSearch} />
+          <SearchIcon onClick={searchBoxToggle} />
         </IconBox>
       )}
     </>
